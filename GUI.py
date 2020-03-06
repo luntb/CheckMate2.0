4d151867e707bcc257665321b1bb407d04251a23
audreybsnyder <audrey.snyder@byu.edu>
2020-03-05 20:05:19 -0700

    Can now open chessboard gui from main gui and go back to main gui after playing chess

--- GUI.py
+++ GUI.py
@@ -11,6 +11,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 import run
 import os
+from threading import Thread
 import sys
 sys.path.insert(0, '/home/pi/Documents/CheckMate2.0/chessgamesimple')
 from chessgamesimple import ChessBoardGUI
@@ -29,6 +30,13 @@ class MainWindow(Screen):
 
 
 class PlayChessWindow(Screen):
+    def on_enter(self):
+        print('start game')
+        x = Thread(target=ChessBoardGUI.The_Game_GUI)
+        x.start()
+        
+    def quit_the_game(self):
+        ChessBoardGUI.quit_the_game()
     pass
 
 
