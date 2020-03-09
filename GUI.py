cac8ece0d3584cdb4f0134561774bf3423ecb38e
audreybsnyder <audrey.snyder@byu.edu>
2020-03-09 17:38:52 -0600

    Attempting to get chess gui subfolder into branch

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 # Uncomment these next two lines to force Kivy to open fullscreen (what we will want when we are ready for production)
-# from kivy.config import Config
-# Config.set('graphics', 'fullscreen', 'auto')
+from kivy.config import Config
+Config.set('graphics', 'fullscreen', 'auto')
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -15,7 +15,7 @@ from threading import Thread
 import sys
 sys.path.insert(0, '/home/pi/Documents/CheckMate2.0/chessgamesimple')
 from chessgamesimple import ChessBoardGUI
-#from chessgamesimple import chess
+##from chessgamesimple import chess
 
 print("kivy")
 run.main()
